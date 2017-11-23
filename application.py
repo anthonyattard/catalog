cb5ef3f14fb317d434d1f07724b92040c44e5511
Anthony Attard <anthony@anthonyattard.com>
2017-11-22 17:50:01 -0800

    Removed most of the output message when a user logs in with Facebook

--- application.py
+++ application.py
@@ -209,17 +209,10 @@ def fbconnect():
     user_id = getUserID(login_session['email'])
     if not user_id:
         user_id = createUser(login_session)
-    login_session['user_id'] = user_id
+    login_session['user_id'] = user_id    
 
     output = ''
-    output += '<h1>Welcome, '
-    output += login_session['username']
-
-    output += '!</h1>'
-    output += '<img src="'
-    output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-
+    output += 'Welcome'
     flash("Now logged in as %s" % login_session['username'])
     print login_session
     return output
