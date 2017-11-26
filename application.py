098dfa6ffcc1cf24407e6c9bda9cbb03a6a7fa4f
Anthony Attard <anthony@anthonyattard.com>
2017-11-25 20:07:27 -0800

    Added form functionality to newItem() in application.py

--- application.py
+++ application.py
@@ -9,6 +9,7 @@ import httplib2
 from flask import session as login_session
 from flask import (Flask, abort, flash, g, jsonify, make_response, redirect,
                    render_template, request, url_for)
+from forms import ItemForm
 from models import Base, Category, Item, User
 from oauth2client.client import FlowExchangeError, flow_from_clientsecrets
 from sqlalchemy import create_engine, desc
@@ -310,22 +311,31 @@ def newItem():
 
     categories = session.query(Category).all()
 
+    # This loads the ItemForm into a local variable
+    form = ItemForm(request.form)
+
     if request.method == 'GET':
         # This will render a form to add a new item
-        return render_template('newitem.html', categories=categories)
+        return render_template('newitem.html', categories=categories, form=form)
     if request.method == 'POST':
-        # This will add a new item to the database
-        category = session.query(Category).filter_by(
-                   name=request.form['category']).one()
+        # This checks whether the form passes validation
+        if form.validate():
+            # This will add a new item to the database
+            category = session.query(Category).filter_by(
+                       name=request.form['category']).one()
 
-        item = Item(name=request.form['name'],
-                    description=request.form['description'],
-                    category=category,
-                    user_id=login_session['user_id'])
-        session.add(item)
-        session.commit()
+            item = Item(name=request.form['name'],
+                        description=request.form['description'],
+                        category=category,
+                        user_id=login_session['user_id'])
+            session.add(item)
+            session.commit()
 
-        return redirect(url_for('showHome'))
+            return redirect(url_for('showHome'))
+        else:
+            # This will run if the form fails validation
+            return render_template('newitem.html', categories=categories, form=form)
+
 
 
 @app.route('/catalog/<int:item_id>/edit', methods=['GET', 'POST'])
